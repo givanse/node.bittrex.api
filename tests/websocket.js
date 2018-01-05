98acdf85f1f80ff119cd0ae98e4880b6570128ee
Roman Lakhtadyr <roman@Romans-MacBook-Pro.local>
2018-01-05 11:42:41 +0200

    Fix backward compatibility

--- tests/websocket.js
+++ tests/websocket.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var bittrex = require('../node.bittrex.api.js')();
+var bittrex = require('../node.bittrex.api.js');
 
 describe('Bittrex websocket API', function() {
 
